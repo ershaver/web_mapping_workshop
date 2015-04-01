b65e33528c3d614ace8ae844d3896227d81aeee3
ershaver <ershaver@gwmail.gwu.edu>
2015-03-31 20:04:47 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
+var mapId = 'ershaver.d98b40d0'; //<- this references the ugly green map that I made
 var token = 'pk.eyJ1IjoiZXJzaGF2ZXIiLCJhIjoiejlGRGhSQSJ9.zh1_V1SIW2JmjQTMouLs_Q'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
