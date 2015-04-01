4a63e190c6521c6f0da18bf870ba8867eb580e8c
ershaver <ershaver@gwmail.gwu.edu>
2015-03-31 20:31:34 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -10,3 +10,16 @@ var map = L.mapbox.map('map', mapId);
 
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
+
+var dataFiletoAdd = 'data/parks.geojson';
+
+vart featureLayer = L.mapbox.featureLayer().loadURL(dataFiletoAdd).addTo(map)
+
+featureLayer.on('ready',function(){
+  this.setStyle({
+    'color':'#ec008c',
+    'weight':4,
+    'opacity':.7
+  });
+  map.fitBounds(featureLayer.getBounds());
+});
